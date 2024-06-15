f528f3d2e67700a59f5092fee4a40906c58fa8d9
TShAdowL <pablo.joel.morales.hernandez@gmail.com>
2024-06-14 22:24:44 -0600

    Ejercicio 2 Lucian's Luscious Lasagna
    https://exercism.org/tracks/csharp/exercises/lucians-luscious-lasagna

--- 2024/001HelloWorld.cs
+++ 2024/001HelloWorld.cs
@@ -1,4 +1,21 @@
-public static class HelloWorld
-{
+/*
+https://exercism.org/tracks/csharp/exercises/hello-world
+
+
+Instructions
+The classical introductory exercise. Just say "Hello, World!".
+
+"Hello, World!" is the traditional first program for beginning programming in a new language or environment.
+
+The objectives are simple:
+
+Modify the provided code so that it produces the string "Hello, World!".
+Run the test suite and make sure that it succeeds.
+Submit your solution and check it at the website.
+If everything goes well, you will be ready to fetch your first real exercise.
+*/
+
+
+public static class HelloWorld {
     public static string Hello() => "Hello, World!";
 }
